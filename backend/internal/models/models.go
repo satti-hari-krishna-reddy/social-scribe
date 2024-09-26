edf810cfc90859e9a4131b8bde407ca40539699a
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-09-26 21:29:47 +0530

    add User struct

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -1,6 +1,20 @@
 package models
 
-
+type User struct {
+	Id                 string
+	UserName           string
+	PassWord           string
+	Session            string
+	ApiKey             string
+	Verified           bool
+	WebSocketUrl       string
+	XoauthKey          string
+	LinkedInOauthKey   string
+	HashnodePAT        string
+	SharedPosts        []SharedPost
+	ScheduledPosts     [] ScheduledPost
+	Notifications      []string
+}
 
 type SharedPost struct {
 	Id                  string
