15b760215168f6a98b7ac82dcd911d9a82922022
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2024-10-16 23:27:18 +0530

    added bson tags for mongo interaction

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -1,35 +1,51 @@
 package models
 
+import (
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    "time"
+)
+
 type User struct {
-	Id                 string
-	UserName           string
-	PassWord           string
-	Session            string
-	ApiKey             string
-	Verified           bool
-	WebSocketUrl       string
-	XoauthKey          string
-	LinkedInOauthKey   string
-	HashnodePAT        string
-	SharedPosts        []SharedPost
-	ScheduledPosts     [] ScheduledPost
-	Notifications      []string
+    Id                 primitive.ObjectID `bson:"_id,omitempty"`
+    UserName           string             `bson:"username"`
+    PassWord           string             `bson:"password"`
+    Verified           bool               `bson:"verified"`
+    WebSocketUrl       string             `bson:"websocket_url"`
+    XoauthKey          string             `bson:"xoauth_key"`
+    LinkedInOauthKey   string             `bson:"linkedin_oauth_key"`
+    HashnodePAT        string             `bson:"hashnode_pat"`
+    SharedBlogs        []SharedBlog       `bson:"shared_posts"`
+    ScheduledBlogs     []ScheduledBlog    `bson:"scheduled_posts"`
+    Notifications      []string           `bson:"notifications"`
 }
 
-type SharedPost struct {
-	Id                  string
-	Title               string
-	Brief               string
-	Url                 string
-	Date                string
-	Platforms           []string
+type Session struct {
+    PartitionKey string    `bson:"partition_key"`
+    RowKey       string    `bson:"row_key"`
+    UserID       string    `bson:"user_id"`
+    LastActive   time.Time `bson:"last_active"`
+    ExpiresAt    time.Time `bson:"expires_at"`
 }
 
-type ScheduledPost struct {
-	Id                  string
-	Title               string
-	Brief               string
-	Url                 string
-	Date                string
-	Platforms           []string
+
+type LoginStruct struct {
+	Username string `bson:"username"`
+	Password string `bson:"password"`
+}
+
+type Blog struct {
+	Id        string
+	Title     string
+	Brief     string
+	Url       string
+	Date      string
+	Platforms []string
+}
+
+type SharedBlog struct {
+	Blog
+}
+
+type ScheduledBlog struct {
+	Blog
 }
