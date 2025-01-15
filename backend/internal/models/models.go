1e5c646ffc633bbc484fc319eb4a92b1cf485276
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-15 11:37:30 +0530

    added validation code related to scheduled blog handler

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -1,51 +1,139 @@
 package models
 
 import (
-    "go.mongodb.org/mongo-driver/bson/primitive"
+    "fmt"
+    "net/url"
+    "strings"
     "time"
+
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-    Id                 primitive.ObjectID `bson:"_id,omitempty"`
-    UserName           string             `bson:"username"`
-    PassWord           string             `bson:"password"`
-    Verified           bool               `bson:"verified"`
-    WebSocketUrl       string             `bson:"websocket_url"`
-    XoauthKey          string             `bson:"xoauth_key"`
-    LinkedInOauthKey   string             `bson:"linkedin_oauth_key"`
-    HashnodePAT        string             `bson:"hashnode_pat"`
-    SharedBlogs        []SharedBlog       `bson:"shared_posts"`
-    ScheduledBlogs     []ScheduledBlog    `bson:"scheduled_posts"`
-    Notifications      []string           `bson:"notifications"`
+    Id               primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+    UserName         string             `json:"username" bson:"username"`
+    PassWord         string             `json:"password" bson:"password"`
+    Verified         bool               `json:"verified" bson:"verified"`
+    WebSocketUrl     string             `json:"websocket_url" bson:"websocket_url"`
+    XoauthKey        string             `json:"xoauth_key" bson:"xoauth_key"`
+    LinkedInOauthKey string             `json:"linkedin_oauth_key" bson:"linkedin_oauth_key"`
+    HashnodePAT      string             `json:"hashnode_pat" bson:"hashnode_pat"`
+    SharedBlogs      []SharedBlog       `json:"shared_posts" bson:"shared_posts"`
+    ScheduledBlogs   []ScheduledBlog    `json:"scheduled_posts" bson:"scheduled_posts"`
+    Notifications    []string           `json:"notifications" bson:"notifications"`
 }
 
 type Session struct {
-    PartitionKey string    `bson:"partition_key"`
-    RowKey       string    `bson:"row_key"`
-    UserID       string    `bson:"user_id"`
-    LastActive   time.Time `bson:"last_active"`
-    ExpiresAt    time.Time `bson:"expires_at"`
+    PartitionKey string    `json:"partition_key" bson:"partition_key"`
+    RowKey       string    `json:"row_key" bson:"row_key"`
+    UserID       string    `json:"user_id" bson:"user_id"`
+    LastActive   time.Time `json:"last_active" bson:"last_active"`
+    ExpiresAt    time.Time `json:"expires_at" bson:"expires_at"`
 }
 
-
 type LoginStruct struct {
-	Username string `bson:"username"`
-	Password string `bson:"password"`
+    Username string `json:"username" bson:"username"`
+    Password string `json:"password" bson:"password"`
+}
+
+type ScheduledBlogData struct {
+    UserID        string        `json:"user_id" bson:"user_id"`
+    ScheduledBlog ScheduledBlog `json:"blog" bson:"blog"`
 }
 
 type Blog struct {
-	Id        string
-	Title     string
-	Brief     string
-	Url       string
-	Date      string
-	Platforms []string
+    Id                string `json:"id" bson:"id"`
+    Title             string `json:"title" bson:"title"`
+    Url               string `json:"url" bson:"url"`
+    CoverImage        Image  `json:"cover_image" bson:"cover_image"`
+    Author            Author `json:"author" bson:"author"`
+    ReadTimeInMinutes int    `json:"read_time_in_minutes" bson:"read_time_in_minutes"`
+}
+
+type Image struct {
+    URL string `json:"url" bson:"url"`
+}
+
+type Author struct {
+    Name string `json:"name" bson:"name"`
 }
 
 type SharedBlog struct {
-	Blog
+    Blog
+    Platforms  []string  `json:"platforms" bson:"platforms"`
+    SharedTime string    `json:"shared_time" bson:"shared_time"`
 }
 
 type ScheduledBlog struct {
-	Blog
+    Blog
+    Platforms     []string  `json:"platforms" bson:"platforms"`
+    ScheduledTime  string    `json:"scheduled_time" bson:"scheduled_time"`
+}
+
+func (b *Blog) ValidateBase() error {
+    if strings.TrimSpace(b.Title) == "" {
+        return fmt.Errorf("title is required")
+    }
+
+    if strings.TrimSpace(b.Url) == "" || !isValidURL(b.Url) {
+        return fmt.Errorf("a valid URL is required")
+    }
+
+    if strings.TrimSpace(b.Id) == "" {
+        return fmt.Errorf("ID is required")
+    }
+
+    if strings.TrimSpace(b.Author.Name) == "" {
+        return fmt.Errorf("author name is required")
+    }
+
+    if strings.TrimSpace(b.CoverImage.URL) == "" || !isValidURL(b.CoverImage.URL) {
+        return fmt.Errorf("a valid cover image URL is required")
+    }
+
+    return nil
+}
+
+func (sb *ScheduledBlog) Validate() error {
+
+
+    if err := sb.Blog.ValidateBase(); err != nil {
+        return err
+    }
+
+    if len(sb.Platforms) == 0 {
+        return fmt.Errorf("at least one platform is required")
+    }
+
+    scheduledTime, err := time.Parse(time.RFC3339, sb.ScheduledTime)
+    if err != nil {
+        return fmt.Errorf("invalid scheduled_time format, expected YYYY-MM-DD HH:mm")
+    }
+	currentTime := time.Now()
+	diff := scheduledTime.Sub(currentTime)
+
+	if diff > (7 * 24 * time.Hour) {
+		return fmt.Errorf("scheduled time is more than 7 days from now")
+	} else if diff < 0 {
+		return fmt.Errorf("scheduled time is in the past")
+	} 
+
+    return nil
+}
+
+func (shb *SharedBlog) Validate() error {
+    if err := shb.Blog.ValidateBase(); err != nil {
+        return err
+    }
+
+    if len(shb.Platforms) == 0 {
+        return fmt.Errorf("at least one platform is required")
+    }
+
+    return nil
+}
+
+func isValidURL(str string) bool {
+    u, err := url.Parse(str)
+    return err == nil && u.Scheme != "" && u.Host != ""
 }
