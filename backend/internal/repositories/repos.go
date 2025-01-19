d24d15728b2576b9a7a084d3706fa48cc18f5229
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-19 17:51:02 +0530

    fix logging format and formatting of the code

--- backend/internal/repositories/repos.go
+++ backend/internal/repositories/repos.go
@@ -2,10 +2,10 @@ package repositories
 
 import (
 	"context"
-	"log"
-	"time"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
 )
 
 var client *mongo.Client
@@ -15,11 +15,13 @@ func InitMongoDb() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	dbName := "social-scribe"
+
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	var err error
 	client, err = mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatal("Error connecting to MongoDB:", err)
+		log.Fatal("[ERROR] Failed connecting to MongoDB:", err)
 	}
 
 	err = client.Ping(ctx, nil)
@@ -27,6 +29,6 @@ func InitMongoDb() {
 		log.Fatal("Could not ping MongoDB:", err)
 	}
 
-	userCollection = client.Database("yourDBName").Collection("users")
-	log.Println("Successfully connected to MongoDB")
+	userCollection = client.Database(dbName).Collection("users")
+	log.Println("[INFO] Successfully connected to MongoDB")
 }
