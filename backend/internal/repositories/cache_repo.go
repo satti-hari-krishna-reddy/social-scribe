410d676d47c4e02389e7be5dbb8d7f0acc8efad2
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-27 20:12:22 +0530

    feat : adding cache collection

---
+++ backend/internal/repositories/cache_repo.go
@@ -0,0 +1,72 @@
+package repositories
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"social-scribe/backend/internal/models"
+)
+
+func SetCache(key string, value interface{}, expiration time.Duration) (string, error) {
+	ctx := context.TODO()
+
+	item := models.CacheItem{
+		Key:   key,
+		Value: value,
+	}
+
+	if expiration > 0 {
+		item.ExpiresAt = time.Now().Add(expiration)
+	}
+
+	_, err := cacheCollection.UpdateOne(
+		ctx,
+		bson.M{"key": key},
+		bson.M{"$set": item},
+		options.Update().SetUpsert(true),
+	)
+
+	if err != nil {
+		log.Printf("[ERROR] Error setting cache for key %s: %v", key, err)
+		return "", err
+	}
+
+	return key, nil
+}
+
+func GetCache(key string) (interface{}, bool) {
+	ctx := context.TODO()
+
+	var result models.CacheItem
+	err := cacheCollection.FindOne(ctx, bson.M{"key": key}).Decode(&result)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, false
+		}
+		log.Printf("[ERROR] Error getting cache for key %s: %v", key, err)
+		return nil, false
+	}
+
+	// Double-check expiration in case TTL cleanup hasn't happened yet
+	if !result.ExpiresAt.IsZero() && time.Now().After(result.ExpiresAt) {
+		DeleteCache(key)
+		return nil, false
+	}
+
+	return result.Value, true
+}
+
+func DeleteCache(key string) error {
+	ctx := context.TODO()
+
+	_, err := cacheCollection.DeleteOne(ctx, bson.M{"key": key})
+	if err != nil {
+		log.Printf("[ERROR] Error deleting cache for key %s: %v", key, err)
+	}
+	return err
+}
