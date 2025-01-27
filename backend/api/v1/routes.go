7ae65e6c950694caebc78fa82134000d199537d6
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-27 20:10:09 +0530

    adding shared blog handler and fixing other stuff

--- backend/api/v1/routes.go
+++ backend/api/v1/routes.go
@@ -13,24 +13,23 @@ func RegisterRoutes() *mux.Router {
 
 	apiV1.HandleFunc("/user/signup", handlers.SignupUserHandler).Methods(http.MethodPost)
 	apiV1.HandleFunc("/user/login", handlers.LoginUserHandler).Methods(http.MethodPost)
-	apiV1.HandleFunc("/user/{id}/shared_posts", handlers.GetUserSharedBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
-	apiV1.HandleFunc("/user/{id}/scheduled_posts", handlers.GetUserScheduledBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
-	// apiV1.HandleFunc("/user/posts/{id}", handlers.GetUserBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
+	apiV1.HandleFunc("/user/scheduled_posts", handlers.GetUserScheduledBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
+	// apiV1.HandleFunc("/user/posts", handlers.GetUserBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
 	apiV1.HandleFunc("/user/getinfo", handlers.GetUserInfoHandler).Methods(http.MethodGet)
 
-	apiV1.HandleFunc("/user/{id}/notifications", handlers.GetUserNotificationsHandler).Methods(http.MethodGet, http.MethodOptions)
-	apiV1.HandleFunc("/user/{id}/notifications/clear", handlers.ClearUserNotificationsHandler).Methods(http.MethodDelete, http.MethodOptions)
+	apiV1.HandleFunc("/user/notifications", handlers.GetUserNotificationsHandler).Methods(http.MethodGet, http.MethodOptions)
+	apiV1.HandleFunc("/user/notifications/clear", handlers.ClearUserNotificationsHandler).Methods(http.MethodDelete, http.MethodOptions)
 
-	apiV1.HandleFunc("/blogs/share", handlers.GetUserSharedBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
 	apiV1.HandleFunc("/blogs/schedule", handlers.ScheduleUserBlogHandler).Methods(http.MethodPost, http.MethodOptions)
 	apiV1.HandleFunc("/blogs/schedule/delete", handlers.GetUserSharedBlogsHandler).Methods(http.MethodDelete, http.MethodOptions)
+	apiV1.HandleFunc("/blogs/user/share", handlers.ShareBlogHandler).Methods(http.MethodPost, http.MethodOptions)
+	apiV1.HandleFunc("/blogs/user/shared-blogs", handlers.GetUserSharedBlogsHandler).Methods(http.MethodGet, http.MethodOptions)
 
 	apiV1.HandleFunc("/user/connect-twitter", handlers.ConnectXhandler).Methods(http.MethodGet, http.MethodOptions)
 	apiV1.HandleFunc("/user/twitter-callback", handlers.XcallbackHandler).Methods(http.MethodGet, http.MethodOptions)
-	apiV1.HandleFunc("/user/post-tweet", handlers.PostTweetHandler).Methods(http.MethodPost, http.MethodOptions)
 	apiV1.HandleFunc("/user/connect-linkedin", handlers.ConnectLinkedInHandler).Methods(http.MethodGet, http.MethodOptions)
 	apiV1.HandleFunc("/user/linkedin-callback", handlers.LinkedCallbackHandler).Methods(http.MethodGet, http.MethodOptions)
-	apiV1.HandleFunc("/user/post-linkedin", handlers.LinkedPostHandler).Methods(http.MethodPost, http.MethodOptions)
+	apiV1.HandleFunc("/user/verify-hashnode", handlers.VerifyHashnodeHandler).Methods(http.MethodPost, http.MethodOptions)
 
 	return router
 
