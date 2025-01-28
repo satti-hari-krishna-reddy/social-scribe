e619c199835e722120e1c71591430bcf6d70b601
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-28 23:08:54 +0530

    ading a loader and fall back text when no blogs are avaiable

--- frontend/src/components/BlogGrid.jsx
+++ frontend/src/components/BlogGrid.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, Typography } from '@mui/material';
 import BlogCard from './BlogCard';
 import NotificationBell from './Notifications';
-import BlogSectionTabs from './BlogSection';
 
-const BlogGrid = ({ blogs }) => {
+const BlogGrid = ({ blogs, loading }) => {
   return (
     <>
-<NotificationBell />
-<BlogSectionTabs />
-    <Container maxWidth="lg" sx={{ marginTop: '30px' }}>
-      <Grid container spacing={4}>
-        {blogs.map((blog, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <BlogCard blog={blog} />
+      <NotificationBell />
+      <Container maxWidth="lg" sx={{ marginTop: '30px' }}>
+        {loading ? (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '50vh',
+            }}
+          >
+            <Typography variant="h6" color="white">
+              Loading blogs...
+            </Typography>
+          </div>
+        ) : blogs.length === 0 ? (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '50vh',
+            }}
+          >
+            <Typography variant="h6" color="white">
+              Dude, there are no blogs to show!
+            </Typography>
+          </div>
+        ) : (
+          <Grid container spacing={4}>
+            {blogs.map((blog, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <BlogCard blog={blog} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Container>
+        )}
+      </Container>
     </>
   );
 };
