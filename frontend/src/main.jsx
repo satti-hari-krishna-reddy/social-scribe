40ab3b2e5a6f9460264994b1c6b2e96b6f85c6bb
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-01-29 21:54:55 +0530

    adding toast component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToastContainer position="bottom-right" autoClose={6000} />
     <App />
   </StrictMode>,
 )
