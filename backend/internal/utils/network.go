e5bc6314a48c169b5f916f9eb7bbbc2c6edde779
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-02-15 23:46:03 +0530

    added function to get the client ip

---
+++ backend/internal/utils/network.go
@@ -0,0 +1,26 @@
+
+package utils
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+func GetClientIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-Forwarded-For")
+	if forwarded != "" {
+		ips := strings.Split(forwarded, ",")
+		return strings.TrimSpace(ips[0]) 
+	}
+
+	// Check X-Real-IP header
+	realIP := r.Header.Get("X-Real-IP")
+	if realIP != "" {
+		return realIP
+	}
+
+	// Hmmm, incase we're behind a proxy, let's get the IP from the remote address
+	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+	return ip
+}
