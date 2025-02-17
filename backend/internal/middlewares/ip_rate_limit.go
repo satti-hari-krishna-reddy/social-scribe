a26c04f0d7d544745474881a4666425412f12aa5
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-02-17 18:23:54 +0530

    rewriting with better return response

--- backend/internal/middlewares/ip_rate_limit.go
+++ backend/internal/middlewares/ip_rate_limit.go
@@ -11,7 +11,8 @@ func IPRateLimitMiddleware(limit int, duration time.Duration) func(http.Handler)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if services.IsIPRateLimited(r, limit, duration) {
-				http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+				w.WriteHeader(http.StatusTooManyRequests)
+				w.Write([]byte(`{"success": false, "reason": "Rate limit exceeded"}`))
 				return
 			}
 			next.ServeHTTP(w, r)
