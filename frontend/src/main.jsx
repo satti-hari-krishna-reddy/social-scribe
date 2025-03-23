87872669bd5633950947678f10566958dbe5c1f3
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-03-23 22:31:02 +0530

    fix prettier formatting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -9,5 +9,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastContainer position="bottom-right" autoClose={6000} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
