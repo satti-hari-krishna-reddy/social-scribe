c6b3f1c7fde687fb743e5b133e5d6f3402eda152
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-03-23 22:32:10 +0530

    fix prettier formatting

--- frontend/src/components/SignUpModal.jsx
+++ frontend/src/components/SignUpModal.jsx
@@ -7,11 +7,10 @@ const SignUpModal = ({ open, handleClose, setIsLoggedIn, setUser, apiUrl }) => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleSignUp = async() => {
-
+  const handleSignUp = async () => {
     if (password !== confirmPassword) {
       alert('Passwords do not match!');
       return;
@@ -20,42 +19,41 @@ const SignUpModal = ({ open, handleClose, setIsLoggedIn, setUser, apiUrl }) => {
       const response = await fetch(apiUrl + '/api/v1/user/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 
-          username : email, 
-          password: password 
+        body: JSON.stringify({
+          username: email,
+          password: password,
         }),
         credentials: 'include',
-      })
+      });
       if (response.status === 409) {
         setError('User already exists');
         return;
       }
       const data = await response.json();
       if (response.ok) {
-     
-      setUser(data);
-      setIsLoggedIn(true);
-      navigate('/verification');
-    } }
-      catch (error) {
-        console.error('Error fetching user:', error);
-        setUser(null);
-        setError('Error occurred');
+        setUser(data);
+        setIsLoggedIn(true);
+        navigate('/verification');
       }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      setUser(null);
+      setError('Error occurred');
+    }
   };
   return (
     <Modal
       open={open}
       onClose={handleClose}
-      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} 
+      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
     >
-      <Box 
-        sx={{ 
-          backgroundColor: '#2E2E2E', 
-          borderRadius: '8px', 
-          padding: '2rem', 
-          width: { xs: '90%', sm: '400px' }, 
-          boxShadow: 24 
+      <Box
+        sx={{
+          backgroundColor: '#2E2E2E',
+          borderRadius: '8px',
+          padding: '2rem',
+          width: { xs: '90%', sm: '400px' },
+          boxShadow: 24,
         }}
       >
         <Typography variant="h6" gutterBottom sx={{ color: '#FF6B6B', textAlign: 'center' }}>
@@ -68,7 +66,7 @@ const SignUpModal = ({ open, handleClose, setIsLoggedIn, setUser, apiUrl }) => {
           fullWidth
           sx={{ marginBottom: '1rem' }}
           InputLabelProps={{
-            style: { color: '#FFFFFF' }
+            style: { color: '#FFFFFF' },
           }}
           InputProps={{
             style: { color: '#FFFFFF' },
@@ -86,7 +84,7 @@ const SignUpModal = ({ open, handleClose, setIsLoggedIn, setUser, apiUrl }) => {
           fullWidth
           sx={{ marginBottom: '1rem' }}
           InputLabelProps={{
-            style: { color: '#FFFFFF' } 
+            style: { color: '#FFFFFF' },
           }}
           InputProps={{
             style: { color: '#FFFFFF' },
@@ -104,39 +102,46 @@ const SignUpModal = ({ open, handleClose, setIsLoggedIn, setUser, apiUrl }) => {
           fullWidth
           sx={{ marginBottom: '1rem' }}
           InputLabelProps={{
-            style: { color: '#FFFFFF' } 
+            style: { color: '#FFFFFF' },
           }}
           InputProps={{
-            style: { color: '#FFFFFF' }, 
+            style: { color: '#FFFFFF' },
             placeholder: 'Re-enter your password',
           }}
           placeholder="Re-enter your password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        {error && <Typography color="red" variant="body2">{error}</Typography>}
+        {error && (
+          <Typography color="red" variant="body2">
+            {error}
+          </Typography>
+        )}
 
-        <Button 
-          variant="contained" 
-          onClick={handleSignUp} 
-          sx={{ 
-            backgroundColor: '#FF6B6B', 
-            color: '#FFFFFF', 
-            fontWeight: 'bold', 
-            width: '100%' 
+        <Button
+          variant="contained"
+          onClick={handleSignUp}
+          sx={{
+            backgroundColor: '#FF6B6B',
+            color: '#FFFFFF',
+            fontWeight: 'bold',
+            width: '100%',
           }}
         >
           Sign Up
         </Button>
 
-        <Typography 
-          sx={{ 
-            marginTop: '1rem', 
-            textAlign: 'center', 
-            color: '#FFFFFF' 
+        <Typography
+          sx={{
+            marginTop: '1rem',
+            textAlign: 'center',
+            color: '#FFFFFF',
           }}
         >
-          Already have an account? <span onClick={handleClose} style={{ color: '#FF6B6B', cursor: 'pointer' }}>Login</span>
+          Already have an account?{' '}
+          <span onClick={handleClose} style={{ color: '#FF6B6B', cursor: 'pointer' }}>
+            Login
+          </span>
         </Typography>
       </Box>
     </Modal>
