26e147fbe5a8cc171746effa5b642dfc3f770742
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-03-24 18:53:22 +0530

    add csrf token extraction for login and signin

--- frontend/src/views/MainPage.jsx
+++ frontend/src/views/MainPage.jsx
@@ -6,10 +6,10 @@ import CallToAction from '../components/CallToAction';
 import Footer from '../components/Footer';
 import About from '../components/About';
 
-function MainPage({ setIsLoggedIn, setUser, apiUrl }) {
+function MainPage({ setIsLoggedIn, setUser, apiUrl, csrfToken, setCsrfToken }) {
   return (
     <div>
-      <HeroSection setIsLoggedIn={setIsLoggedIn} setUser={setUser} apiUrl={apiUrl} />
+      <HeroSection setIsLoggedIn={setIsLoggedIn} setUser={setUser} apiUrl={apiUrl} csrfToken={csrfToken} setCsrfToken={setCsrfToken} />
       <HowItWorks />
       <CallToAction />
       <Features />
