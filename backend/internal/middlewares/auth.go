d1d304bf135341e66129a3a0cbecfc44a471ec1f
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-03-24 15:25:37 +0530

    added CSRF checking in auth middle ware

--- backend/internal/middlewares/auth.go
+++ backend/internal/middlewares/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"time"
+	"fmt"
 
 	"social-scribe/backend/internal/models"
 	repo "social-scribe/backend/internal/repositories"
@@ -21,41 +22,71 @@ func AuthMiddleware(limit int, duration time.Duration, next http.Handler) http.H
 		// Validate session
 		cookie, err := r.Cookie("session_token")
 		if err != nil {
-			http.Error(w, "Unauthorized: Missing session token", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"success": false, "reason": "Unauthorized: Missing session token"}`))
 			return
 		}
 
 		sessionData, exists := repo.GetCache(cookie.Value)
 		if !exists {
-			http.Error(w, "Unauthorized: Invalid or expired session", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"success": false, "reason": "Unauthorized: Invalid or expired session"}`))
 			return
 		}
 
 		session, ok := sessionData.(models.CacheItem)
 		if !ok {
-			http.Error(w, "Unauthorized: Invalid session format", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"success": false, "reason": "Unauthorized: Invalid session format"}`))
 			return
 		}
 
 		if session.ExpiresAt.Before(time.Now()) {
-			http.Error(w, "Unauthorized: Session expired", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"success": false, "reason": "Unauthorized: Session expired"}`))
 			return
 		}
 
 		oid, ok := session.Value.(primitive.ObjectID)
 		if !ok {
-			http.Error(w, "Unauthorized: Invalid user ID format", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"success": false, "reason": "Unauthorized: Invalid user ID format"}`))
 			return
 		}
 
 		userID := oid.Hex()
 
+		// checking for rate limiting
 		if repo.IsRateLimited(userID, limit, duration) {
-			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+			w.WriteHeader(http.StatusTooManyRequests)
+			w.Write([]byte(`{"success": false, "reason": "Too Many Requests"}`))
+			return
+		}
+
+		// check for CSRF
+		cacheKey := fmt.Sprintf("CSRF_%s", userID)
+		tokenData, exists := repo.GetCache(cacheKey)
+		if !exists {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte(`{"success": false, "reason": "CSRF token missing"}`))
+			return
+		}
+
+		tokenInfo, _ := tokenData.(models.CacheItem)
+		expectedToken := tokenInfo.Value
+
+		// Get the token from the request header
+		requestToken := r.Header.Get("X-CSRF-Token")
+		if requestToken == "" || requestToken != expectedToken {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte(`{"success": false, "reason": "Invalid CSRF token"}`))
 			return
 		}
 
 		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
+		w.Header().Set("Permissions-Policy", "geolocation=(), microphone=(), camera=(), usb=()")
+
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
