e685d593ec7de2979d7670329660383e59359d25
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-03-27 19:52:00 +0530

    fix entrypoint

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
-envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
-exec nginx -g "daemon off;"
+envsubst '$BACKEND_URL' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
+exec "$@"
